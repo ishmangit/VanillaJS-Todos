9837793976274494ffbb197402e90fb91c8b6025
ishman <ishman99@gmail.com>
2020-07-09 13:02:47 +0200

    Creación de clase todoList y fichero index de clases

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './styles.css';
-import { Todo } from './classes/todo.class';
+import { Todo, TodoList } from './classes';
 
+const todoList = new TodoList();
 const tarea = new Todo('Aprender Javascript');
-console.log(tarea);
+
+todoList.nuevoTodo(tarea);
+
+console.log(todoList);
